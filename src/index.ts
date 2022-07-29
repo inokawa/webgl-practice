13dc6ef532ae4401567c504fec0f7eea499a53e5
inokawa <48897392+inokawa@users.noreply.github.com>
2022-07-29 10:47:06 +0900

    1.5

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-alert("hello");
+window.onload = () => {
+  const canvas = document.getElementById("app") as HTMLCanvasElement | null;
+  if (!canvas) {
+    console.error("Sorry! No HTML5 Canvas was found on this page");
+    return;
+  }
+
+  const gl = canvas.getContext("webgl2");
+  const message = gl ? "You got a WebGL2 context" : "WebGl is not available";
+
+  alert(message);
+};
