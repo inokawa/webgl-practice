5e95b0a30db59c0032efdf15f4773ee8db86cd07
inokawa <48897392+inokawa@users.noreply.github.com>
2022-07-29 12:38:49 +0900

    1.10

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,44 @@
-import { init } from "./app";
+import * as utils from "./utils";
+
+let gl: WebGL2RenderingContext;
+
+const updateClearColor = (...color: [number, number, number, number]) => {
+  gl.clearColor(...color);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.viewport(0, 0, 0, 0);
+};
+
+const checkKey = (e: KeyboardEvent) => {
+  switch (e.key) {
+    case "1":
+      updateClearColor(0.2, 0.8, 0.2, 1.0);
+      break;
+    case "2":
+      updateClearColor(0.2, 0.2, 0.8, 1.0);
+      break;
+    case "3":
+      updateClearColor(Math.random(), Math.random(), Math.random(), 1.0);
+      break;
+    case "4": {
+      const color = gl.getParameter(gl.COLOR_CLEAR_VALUE);
+      alert(
+        `clearColor = (${color[0].toFixed(1)}, ${color[1].toFixed(
+          1
+        )}, ${color[2].toFixed(1)})`
+      );
+      window.focus();
+      break;
+    }
+  }
+};
 
 window.onload = () => {
-  const canvas = document.getElementById("app") as HTMLCanvasElement | null;
-  if (!canvas) {
-    console.error("Sorry! No HTML5 Canvas was found on this page");
-    return;
-  }
+  const canvas = utils.getCanvas("app");
+  if (!canvas) return;
+  const ctx = utils.getGLContext(canvas);
+  if (!ctx) return;
 
-  const gl = canvas.getContext("webgl2");
-  if (!gl) {
-    alert("WebGl is not available");
-    return;
-  }
-  alert("You got a WebGL2 context");
+  gl = ctx;
 
-  init(gl);
+  window.onkeydown = checkKey;
 };
