c8a9854d3cf891981c250a0d5e82496a8696e465
inokawa <inokawa@users.noreply.github.com>
2022-08-04 17:21:43 +0000

    deploy: b54161461f9e84fecc475863961e5471b0bf29a0

---
+++ assets/plane.d8c65b5c.js
@@ -0,0 +1,1 @@
+const e=[-50,-5,-50,50,-5,-50,-50,-5,50,50,-5,50],s=[0,1,2,3,2,1],c=[.17,.19,.14,1],t=[.1,.1,.1,1],n=[0,0,0,1],i={vertices:e,indices:s,ambient:c,diffuse:t,specular:n};export{c as ambient,i as default,t as diffuse,s as indices,n as specular,e as vertices};
