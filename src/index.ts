2c54a7f42f8fe73bb45e2f58b3b17b95f340a48b
inokawa <48897392+inokawa@users.noreply.github.com>
2022-08-13 22:21:52 +0900

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,3 @@
-import { init as init26 } from "./chapters/2.6";
-import { init as init2 } from "./chapters/2";
-import { init as init3goraudlambert } from "./chapters/3-goraud-lambert";
-import { init as init3goraudPhong } from "./chapters/3-goraud-phong";
-import { init as init3PhongPhong } from "./chapters/3-phong-phong";
-import { init as init312 } from "./chapters/3.12";
-import { init as init313 } from "./chapters/3.13";
-import { init as init48 } from "./chapters/4.8";
-import { init as init410 } from "./chapters/4.10";
-import { init as init57 } from "./chapters/5.7";
-import { init as init59 } from "./chapters/5.9";
-import { init as init512 } from "./chapters/5.12";
-import { init as init63 } from "./chapters/6.3";
-import { init as init66 } from "./chapters/6.6";
-import { init as init68 } from "./chapters/6.8";
-import { init as init612 } from "./chapters/6.12";
-import { init } from "./chapters/6.14";
 import * as utils from "./utils";
 
 const appendMenu = (menuEl: HTMLElement, text: string, onClick: () => void) => {
@@ -34,75 +17,75 @@ window.onload = async () => {
   const gl = utils.getGLContext(canvas);
   if (!gl) return;
 
-  let dispose: () => void = await init(gl);
+  let dispose: () => void = await (await import("./chapters/6.14")).init(gl);
 
   const menuEl = document.getElementById("menu")!;
   appendMenu(menuEl, "2.6", async () => {
     dispose();
-    dispose = await init26(gl);
+    dispose = await (await import("./chapters/2.6")).init(gl);
   });
   appendMenu(menuEl, "2", async () => {
     dispose();
-    dispose = await init2(gl);
+    dispose = await (await import("./chapters/2")).init(gl);
   });
   appendMenu(menuEl, "3.goraud-lambert", async () => {
     dispose();
-    dispose = await init3goraudlambert(gl);
+    dispose = await (await import("./chapters/3-goraud-lambert")).init(gl);
   });
   appendMenu(menuEl, "3.goraud-phong", async () => {
     dispose();
-    dispose = await init3goraudPhong(gl);
+    dispose = await (await import("./chapters/3-goraud-phong")).init(gl);
   });
   appendMenu(menuEl, "3.phong-phong", async () => {
     dispose();
-    dispose = await init3PhongPhong(gl);
+    dispose = await (await import("./chapters/3-phong-phong")).init(gl);
   });
   appendMenu(menuEl, "3.12", async () => {
     dispose();
-    dispose = await init312(gl);
+    dispose = await (await import("./chapters/3.12")).init(gl);
   });
   appendMenu(menuEl, "3.13", async () => {
     dispose();
-    dispose = await init313(gl);
+    dispose = await (await import("./chapters/3.13")).init(gl);
   });
   appendMenu(menuEl, "4.8", async () => {
     dispose();
-    dispose = await init48(gl);
+    dispose = await (await import("./chapters/4.8")).init(gl);
   });
   appendMenu(menuEl, "4.10", async () => {
     dispose();
-    dispose = await init410(gl);
+    dispose = await (await import("./chapters/4.10")).init(gl);
   });
   appendMenu(menuEl, "5.7", async () => {
     dispose();
-    dispose = await init57(gl);
+    dispose = await (await import("./chapters/5.7")).init(gl);
   });
   appendMenu(menuEl, "5.9", async () => {
     dispose();
-    dispose = await init59(gl);
+    dispose = await (await import("./chapters/5.9")).init(gl);
   });
   appendMenu(menuEl, "5.12", async () => {
     dispose();
-    dispose = await init512(gl);
+    dispose = await (await import("./chapters/5.12")).init(gl);
   });
   appendMenu(menuEl, "6.3", async () => {
     dispose();
-    dispose = await init63(gl);
+    dispose = await (await import("./chapters/6.3")).init(gl);
   });
   appendMenu(menuEl, "6.6", async () => {
     dispose();
-    dispose = await init66(gl);
+    dispose = await (await import("./chapters/6.6")).init(gl);
   });
   appendMenu(menuEl, "6.8", async () => {
     dispose();
-    dispose = await init68(gl);
+    dispose = await (await import("./chapters/6.8")).init(gl);
   });
   appendMenu(menuEl, "6.12", async () => {
     dispose();
-    dispose = await init612(gl);
+    dispose = await (await import("./chapters/6.12")).init(gl);
   });
   appendMenu(menuEl, "6.14", async () => {
     dispose();
-    dispose = await init(gl);
+    dispose = await (await import("./chapters/6.14")).init(gl);
   });
 };
