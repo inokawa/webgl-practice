f9c944a921d301cf04dbe1e395c5a57ec209185c
inokawa <inokawa@users.noreply.github.com>
2022-08-21 16:52:44 +0000

    deploy: 7959d41dd8c47214fe189ffb7c82dc67077ff03e

---
+++ assets/index.54286ed7.js
@@ -0,0 +1,56 @@
+import{c,d as f}from"./webgl.0c8613b1.js";import{S as v}from"./Scene.aa3140c1.js";import{l as m,c as d}from"./index.15e2cb8d.js";import{C as h,a as p}from"./Controls.d2d80294.js";import{T as x}from"./Transforms.dee64e8a.js";import"./mat4.1972b750.js";const C=`#version 300 es
+precision mediump float;
+
+uniform bool uUseVertexColor;
+uniform vec4 uMaterialDiffuse;
+uniform vec4 uMaterialAmbient;
+uniform float uAlpha;
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+uniform mat4 uNormalMatrix;
+uniform vec3 uLightPosition;
+uniform vec4 uLightAmbient;
+uniform vec4 uLightDiffuse;
+uniform bool uUseLambert;
+
+in vec3 aVertexPosition;
+in vec3 aVertexNormal;
+in vec4 aVertexColor;
+
+out vec4 vColor;
+
+void main(void) {
+    vec4 vertex = uModelViewMatrix * vec4(aVertexPosition, 1.0);
+
+    float lambertTerm = 1.0;
+
+    if (uUseLambert) {
+        vec3 normal = vec3(uNormalMatrix * vec4(aVertexNormal, 1.0));
+        vec3 lightDirection = normalize(-uLightPosition);
+        lambertTerm = max(dot(normal, -lightDirection), 0.20);
+    }
+
+    vec4 Ia = uLightAmbient * uMaterialAmbient;
+    vec4 Id = vec4(0.0);
+
+    if (uUseVertexColor) {
+        Id = uLightDiffuse * aVertexColor * lambertTerm;
+    }
+    else {
+        Id = uLightDiffuse * uMaterialDiffuse * lambertTerm;
+    }
+
+    vColor = vec4(vec3(Ia + Id), uAlpha);
+    gl_Position = uProjectionMatrix * uModelViewMatrix * vec4(aVertexPosition, 1.0);
+}`,b=`#version 300 es
+precision mediump float;
+
+uniform vec4 uMaterialDiffuse;
+
+in vec4 vColor;
+
+out vec4 fragColor;
+
+void main(void) {
+    fragColor = vColor;
+}`,D=async e=>{e.clearColor(.9,.9,.9,1),e.clearDepth(100),e.enable(e.DEPTH_TEST),e.depthFunc(e.LEQUAL);const o=c(e,C,b,["aVertexPosition","aVertexNormal","aVertexColor"],["uModelViewMatrix","uProjectionMatrix","uNormalMatrix","uLightPosition","uLightAmbient","uLightDiffuse","uMaterialAmbient","uMaterialDiffuse","uAlpha","uUseLambert","uUseVertexColor"]);let s=!1;const i=new v(e,o);i.add(await m("/models/cube-simple.json"),"simpleCube",{hidden:!1}),i.add(await m("/models/cube-complex.json"),"complexCube",{hidden:!1});const t=new h("ORBITING_TYPE");t.goHome([0,0,3]),t.setFocus([0,0,0]),t.setAzimuth(45),t.setElevation(-30),new p(t,e.canvas);const a=new x(o,t,e.canvas);o.use(),o.setUniform("uLightPosition","vec3",[0,5,20]),o.setUniform("uLightAmbient","vec4",[1,1,1,1]),o.setUniform("uLightDiffuse","vec4",[1,1,1,1]),o.setUniform("uAlpha","float",1),o.setUniform("uUseVertexColor","bool",s),o.setUniform("uUseLambert","bool",!0);const l=d({Lambert:{value:!0,onChange:n=>o.setUniform("uUseLambert","bool",n)},"Per Vertex":{value:s,onChange:n=>s=n},"Complex Cube":{value:!0,onChange:n=>{const r=i.get("simpleCube"),u=i.get("complexCube");n?(r.hidden=!0,u.hidden=!1):(r.hidden=!1,u.hidden=!0)}},"Alpha Value":{value:1,min:0,max:1,step:.1,onChange:n=>o.setUniform("uAlpha","float",n)}});return i.start(n=>{e.viewport(0,0,e.canvas.width,e.canvas.height),e.clear(e.COLOR_BUFFER_BIT|e.DEPTH_BUFFER_BIT),a.updatePerspective();try{n.forEach(r=>{r.hidden||(a.calculateModelView(),a.push(),a.setMatrixUniforms(),a.pop(),o.setUniform("uUseVertexColor","bool",s),o.setUniform("uMaterialDiffuse","vec4",r.diffuse),o.setUniform("uMaterialAmbient","vec4",r.ambient),f(e,r.vao,"TRIANGLES"))})}catch(r){console.error(r)}}),()=>{i.dispose(),l()}};export{D as init};
---
+++ assets/index.d324205b.js
@@ -0,0 +1,85 @@
+import{c as D,d as N}from"./webgl.0c8613b1.js";import{S as U}from"./Scene.aa3140c1.js";import{g as R,s as p,i as b,c as C,l as F}from"./index.15e2cb8d.js";import{C as L,a as P}from"./Controls.d2d80294.js";import{T as A}from"./Transforms.dee64e8a.js";import{F as w}from"./Floor.7b1674dd.js";import{t as k,s as B}from"./mat4.1972b750.js";const _=`#version 300 es
+precision mediump float;
+
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+uniform mat4 uNormalMatrix;
+uniform vec3 uLightPosition;
+uniform vec4 uMaterialDiffuse;
+uniform bool uWireframe;
+
+in vec3 aVertexPosition;
+in vec3 aVertexNormal;
+in vec4 aVertexColor;
+
+out vec3 vNormal;
+out vec3 vLightRay;
+out vec3 vEyeVector;
+out vec4 vFinalColor;
+
+void main(void) {
+  vFinalColor = uMaterialDiffuse;
+
+  vec4 vertex = uModelViewMatrix * vec4(aVertexPosition, 1.0);
+  vNormal = vec3(uNormalMatrix * vec4(aVertexNormal, 1.0));
+
+  vec4 light = vec4(uLightPosition,1.0);
+  vLightRay = vertex.xyz-light.xyz;
+  vEyeVector = -vec3(vertex.xyz);
+
+  gl_Position = uProjectionMatrix * uModelViewMatrix * vec4(aVertexPosition, 1.0);
+}`,y=`#version 300 es
+precision mediump float;
+
+uniform bool uWireframe;
+uniform vec4 uLightAmbient;
+uniform vec4 uLightDiffuse;
+uniform vec4 uMaterialAmbient;
+uniform vec4 uMaterialDiffuse;
+uniform vec4 uPickingColor;
+uniform bool uOffscreen;
+
+in vec3 vNormal;
+in vec3 vLightRay;
+in vec3 vEyeVector;
+in vec4 vFinalColor;
+
+out vec4 fragColor;
+
+void main(void) {
+    if (uOffscreen) {
+        fragColor = uPickingColor;
+        return;
+    }
+
+    if (uWireframe) {
+        fragColor = vFinalColor;
+    }
+    else {
+        // Ambient
+        vec4 Ia = uLightAmbient * uMaterialAmbient;
+
+        // Diffuse
+        vec3 L = normalize(vLightRay);
+        vec3 N = normalize(vNormal);
+        float lambertTerm = max(dot(N, -L), 0.33);
+        vec4 Id = uLightDiffuse * uMaterialDiffuse * lambertTerm;
+
+        // Specular
+        vec3 E = normalize(vEyeVector);
+        vec3 R = reflect(L, N);
+        float specular = pow( max(dot(R, E), 0.5), 50.0);
+        vec4 Is = vec4(0.5) * specular;
+
+        vec4 finalColor = Ia + Id + Is;
+
+        if (uMaterialDiffuse.a != 1.0) {
+            finalColor.a = uMaterialDiffuse.a;
+        }
+        else {
+            finalColor.a = 1.0;
+        }
+
+        fragColor = finalColor;
+    }
+}`;class S{constructor(e,i,a){this.picking=!1,this.gl=e,this.canvas=e.canvas,this.scene=i,this.pickedList=[],this.callbacks=a;const{width:t,height:s}=this.canvas;this.texture=e.createTexture(),e.bindTexture(e.TEXTURE_2D,this.texture),e.texImage2D(e.TEXTURE_2D,0,e.RGBA,t,s,0,e.RGBA,e.UNSIGNED_BYTE,null),this.renderbuffer=e.createRenderbuffer(),e.bindRenderbuffer(e.RENDERBUFFER,this.renderbuffer),e.renderbufferStorage(e.RENDERBUFFER,e.DEPTH_COMPONENT16,t,s),this.framebuffer=e.createFramebuffer(),e.bindFramebuffer(e.FRAMEBUFFER,this.framebuffer),e.framebufferTexture2D(e.FRAMEBUFFER,e.COLOR_ATTACHMENT0,e.TEXTURE_2D,this.texture,0),e.framebufferRenderbuffer(e.FRAMEBUFFER,e.DEPTH_ATTACHMENT,e.RENDERBUFFER,this.renderbuffer),e.bindTexture(e.TEXTURE_2D,null),e.bindRenderbuffer(e.RENDERBUFFER,null),e.bindFramebuffer(e.FRAMEBUFFER,null)}drawToFramebuffer(e){const i=this.gl;i.bindFramebuffer(i.FRAMEBUFFER,this.framebuffer),e(),i.bindFramebuffer(i.FRAMEBUFFER,null)}getHits(){return this.pickedList}update(){const e=this.gl,{width:i,height:a}=this.canvas;e.bindTexture(e.TEXTURE_2D,this.texture),e.texImage2D(e.TEXTURE_2D,0,e.RGBA,i,a,0,e.RGBA,e.UNSIGNED_BYTE,null),e.bindRenderbuffer(e.RENDERBUFFER,this.renderbuffer),e.renderbufferStorage(e.RENDERBUFFER,e.DEPTH_COMPONENT16,i,a)}compare(e,i){return Math.abs(Math.round(i[0]*255)-e[0])<=1&&Math.abs(Math.round(i[1]*255)-e[1])<=1&&Math.abs(Math.round(i[2]*255)-e[2])<=1}find(e){const i=this.gl,a=new Uint8Array(4);i.bindFramebuffer(i.FRAMEBUFFER,this.framebuffer),i.readPixels(e.x,e.y,1,1,i.RGBA,i.UNSIGNED_BYTE,a),i.bindFramebuffer(i.FRAMEBUFFER,null);let t=!1;return this.scene.traverse(s=>{var c,l,d,n,f,u;if(s.alias==="floor")return!1;const v=(l=(c=this.callbacks).hitProperty)==null?void 0:l.call(c,s);if(!v)return!0;if(this.compare(a,v)){const m=this.pickedList.indexOf(s);return~m?(this.pickedList.splice(m,1),(n=(d=this.callbacks).removeHit)==null||n.call(d,s)):(this.pickedList.push(s),(u=(f=this.callbacks).addHit)==null||u.call(f,s)),t=!0}return!1}),t}stop(){var e,i;this.pickedList.length&&((i=(e=this.callbacks).processHits)==null||i.call(e,this.pickedList)),this.pickedList=[]}}const $=async r=>{r.clearColor(.9,.9,.9,1),r.clearDepth(100),r.enable(r.DEPTH_TEST),r.depthFunc(r.LESS),r.blendFunc(r.SRC_ALPHA,r.ONE_MINUS_SRC_ALPHA);const e=D(r,_,y,["aVertexPosition","aVertexNormal","aVertexColor"],["uProjectionMatrix","uModelViewMatrix","uNormalMatrix","uMaterialDiffuse","uMaterialAmbient","uLightAmbient","uLightDiffuse","uLightPosition","uWireframe","uOffscreen","uPickingColor"]),i=new U(r,e),a=async()=>{i.add(new w(80,2));for(let n=0;n<100;n++){const f=Math.floor(Math.random()*2),u={position:V(),scale:I(),diffuse:H(n),pickingColor:x()};switch(f){case 1:i.add(await F("/models/sphere1.json"),`ball_${n}`,u);break;case 0:i.add(await F("/models/cylinder.json"),`cylinder_${n}`,u);break}}};await a();const t=new L("ORBITING_TYPE");t.goHome([0,0,192]),t.setFocus([0,0,0]),t.setElevation(-22),t.setAzimuth(37);const s=new S(r,i,{hitProperty:n=>n.pickingColor,addHit:n=>{n.previous=n.diffuse.slice(0),n.diffuse=n.pickingColor},removeHit:n=>{n.diffuse=n.previous.slice(0)},processHits:n=>{n.forEach(f=>f.diffuse=f.previous)},move:(n,f,u)=>{const m=s.getHits();if(!m)return;const o=Math.max(Math.max(t.position[0],t.position[1]),t.position[2])/2e3;m.forEach(h=>{const M=R(),E=R();u?p(E,t.normal,f*o):(p(E,t.up,-f*o),p(M,t.right,n*o)),b(h.position,h.position,E),b(h.position,h.position,M)})}});new P(t,r.canvas).setPicker(s);const c=new A(e,t,r.canvas);e.use(),e.setUniform("uLightPosition","vec3",[0,5,20]),e.setUniform("uLightAmbient","vec4",[1,1,1,1]),e.setUniform("uLightDiffuse","vec4",[1,1,1,1]);let l=!1;const d=C({"Show Picking Image":{value:l,onChange:n=>l=n},"Reset Scene":async()=>{i.objects=[],await a(),t.goHome(),t.setElevation(-40),t.setAzimuth(-30)}});return i.start(n=>{s.drawToFramebuffer(()=>{e.setUniform("uOffscreen","bool",!0),f(),e.setUniform("uOffscreen","bool",l)}),f();function f(){r.viewport(0,0,r.canvas.width,r.canvas.height),r.clear(r.COLOR_BUFFER_BIT|r.DEPTH_BUFFER_BIT),c.updatePerspective();try{const u=e.getUniform("uOffscreen"),m=l||u;n.forEach(o=>{o.alias==="floor"&&m||(c.calculateModelView(),c.push(),o.alias!=="floor"&&(k(c.modelViewMatrix,c.modelViewMatrix,o.position),B(c.modelViewMatrix,c.modelViewMatrix,o.scale)),c.setMatrixUniforms(),c.pop(),o.diffuse[3]<1&&!u?(r.disable(r.DEPTH_TEST),r.enable(r.BLEND)):(r.enable(r.DEPTH_TEST),r.disable(r.BLEND)),e.setUniform("uMaterialDiffuse","vec4",o.diffuse),e.setUniform("uMaterialAmbient","vec4",o.ambient),e.setUniform("uWireframe","bool",o.wireframe),e.setUniform("uPickingColor","vec4",o.pickingColor||[0,0,0,0]),e.setUniform("uOffscreen","bool",m),N(r,o.vao,o.wireframe?"LINES":"TRIANGLES"))})}catch(u){console.error(u)}}}),()=>{i.dispose(),d()}};function V(){const r=Math.floor(Math.random()*10),e=Math.floor(Math.random()*10);let i=Math.floor(Math.random()*60),a=Math.floor(Math.random()*60);return r>=5&&(i=-i),e>=5&&(a=-a),[i,0,a]}const T={};function x(){const r=[Math.random(),Math.random(),Math.random(),1],e=r.toString();return e in T?x():(T[e]=!0,r)}function H(r){const e=r%30/60+.2;return[e,e,e,1]}function I(){const r=Math.random()+.3;return[r,r,r]}export{$ as init};
